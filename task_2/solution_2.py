f3be081321434d3346e25b86eaae2e13b864af7d
Войтович Екатерина <72070049+EkaterinaVoit@users.noreply.github.com>
2020-10-23 20:41:36 +0300

    Update solution_2.py

--- task_2/solution_2.py
+++ task_2/solution_2.py
@@ -1,4 +1,9 @@
 # your code here
 user_input = input()
-# your code here
+a = 0
+for i in user_input:
+    if len(i) > a:
+        a = len(i)
+most_frequent_character = i      
 print(most_frequent_character)
+#вероятно, код нуждается в доработке
